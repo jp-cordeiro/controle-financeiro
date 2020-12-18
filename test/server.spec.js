eb5740c25eec8e31f471310c610cf559852eec96
JP <jpc.araujo@hotmail.com>
2020-12-18 18:52:05 -0300

    remove  server test

--- test/server.spec.js
+++
@@ -1,7 +0,0 @@
-import supertest from 'supertest';
-const request = supertest('http://localhost:3001');
-
-test('reply on port 3001', async () => {
-  const res = await request.get('/');
-  expect(res.status).toBe(200);
-});
