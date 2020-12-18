df3579c288b4aa19dedd78da65bda77d02678576
JP <jpc.araujo@hotmail.com>
2020-12-18 19:44:35 -0300

    add route users

--- src/app.js
+++ src/app.js
@@ -5,4 +5,15 @@ app.get('/', (req, res) => {
   res.status(200).send();
 });
 
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      name: 'Johnny Silverhand',
+      email: 'johnny@test.com',
+    },
+  ];
+
+  res.status(200).json(users);
+});
+
 module.exports = app;
