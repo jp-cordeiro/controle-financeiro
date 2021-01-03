30ad08d9451f3c56ea8524483faf8afc172d74ce
JP <jpc.araujo@hotmail.com>
2021-01-03 12:32:20 -0300

    refactoring user service and add condition for not repeat user email

--- src/services/users.js
+++ src/services/users.js
@@ -3,8 +3,11 @@ const utils = require('./utils');
 module.exports = app => {
   const requiredProps = ['name', 'email', 'password'];
 
-  const findAll = async () => {
-    const users = await app.db('users').select();
+  const findAll = async (filter = {}) => {
+    const users = await app
+      .db('users')
+      .where(filter)
+      .select();
     return users;
   };
 
@@ -13,6 +16,12 @@ module.exports = app => {
     if (propValidation) {
       return { error: `${propValidation} is a required property` };
     }
+
+    const userDb = await findAll({ email: 'v@test.com' });
+    if (userDb && userDb.length) {
+      return { error: 'There is already a user with that email' };
+    }
+
     const userInserted = await app.db('users').insert(user, '*');
     return userInserted;
   };
